4181273b68aaf5b8a4cfb2bc3f9adee9f251db36
aki <lala.akira@gmail.com>
2018-05-12 00:01:38 +0900

    refactor: fix a bit

--- lib/hanmoto/task.rb
+++ lib/hanmoto/task.rb
@@ -19,7 +19,6 @@ module Hanmoto
       Dir.foreach(Rails.root.join('app', 'views', @view_dir)) do |file|
         next if file.start_with?('.')
         name, format = file.split('.', 3)
-        output_path = Rails.public_path.join([name, format].join('.'))
         layout = @layouts[format.to_sym]
         body = ApplicationController.renderer.render("#{@view_dir}/#{name}", layout: layout)
         path = output_path(name, format)
@@ -28,12 +27,14 @@ module Hanmoto
       end
     end
 
-    def output_path(name, format)
-      Rails.public_path.join([name, ext_by_format(format)].join('.'))
-    end
+    private
 
-    def ext_by_format(format)
-      EXTENSIONS.fetch(format.to_sym, format)
-    end
+      def output_path(name, format)
+        Rails.public_path.join([name, ext_by_format(format)].join('.'))
+      end
+
+      def ext_by_format(format)
+        EXTENSIONS.fetch(format.to_sym, format)
+      end
   end
 end
