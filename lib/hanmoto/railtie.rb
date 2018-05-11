a282f029801400369cbfd1a126df2322e6438f05
aki <lala.akira@gmail.com>
2018-05-11 16:36:19 +0900

    refactor: Rails::Engine -> Rails::Railtie

--- lib/hanmoto/railtie.rb
+++ lib/hanmoto/railtie.rb
@@ -3,12 +3,16 @@
 require 'hanmoto/task'
 
 module Hanmoto
-  class Railtie < Rails::Engine
+  class Railtie < Rails::Railtie
     # options
     config.hanmoto = ActiveSupport::OrderedOptions.new
     config.hanmoto.view_dir = 'public_pages'
     config.hanmoto.layouts = {
       html: 'public',
     }
+
+    rake_tasks do
+      load File.expand_path('../../tasks/hanmoto_tasks.rake', __FILE__)
+    end
   end
 end
