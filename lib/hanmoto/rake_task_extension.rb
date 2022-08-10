fd35bed8ae41b4e819c1d95bde0cf7388b85d43f
Hiroyuki Takamura <takamura1227@gmail.com>
2022-08-10 14:50:22 +0900

    feat: allow disable assets:precompile enhance if ENV['DISABLE_HANMOTO_ON_PRECOMPILE'] is given

--- lib/hanmoto/rake_task_extension.rb
+++ lib/hanmoto/rake_task_extension.rb
@@ -4,10 +4,12 @@ module Hanmoto
   module RakeTaskExtension
     def load_tasks(*args)
       super
-
-      # NOTE: should add at last
-      Rake::Task['assets:precompile'].enhance do
-        Rake::Task['hanmoto:publish'].invoke
+      
+      unless ENV['DISABLE_HANMOTO_ON_PRECOMPILE']
+        # NOTE: should add at last
+        Rake::Task['assets:precompile'].enhance do
+          Rake::Task['hanmoto:publish'].invoke
+        end
       end
 
       self
